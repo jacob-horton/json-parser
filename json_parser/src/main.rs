30cd65def55f6c86b8ef07a4cc596a7a19390d93
Jacob <hortonjacob1@gmail.com>
2025-07-22 17:11:24 +0100

    feat: array parsing + struct for test json blob

--- json_parser/src/main.rs
+++ json_parser/src/main.rs
@@ -9,14 +9,68 @@ mod parser;
 mod scanner;
 mod token;
 
-#[derive(JsonDeserialise, Debug)]
-struct Test {
-    name: Option<String>,
-    age: i64,
+#[derive(Debug, JsonDeserialise)]
+pub struct Root {
+    pub name: String,
+    pub age: i64,
+    pub is_verified: bool,
+    pub balance: f64,
+    pub nickname: Option<String>,
+    pub contact: Contact,
+    pub preferences: Preferences,
+    pub tags: Vec<String>,
+    pub history: Vec<History>,
+    pub unicode_example: String,
+    pub numbers: Numbers,
+}
+
+#[derive(Debug, JsonDeserialise)]
+pub struct Contact {
+    pub email: String,
+    pub phone: String,
+    pub address: Address,
+}
+
+#[derive(Debug, JsonDeserialise)]
+pub struct Address {
+    pub street: String,
+    pub city: String,
+    pub zipcode: String,
+    pub country: String,
+}
+
+#[derive(Debug, JsonDeserialise)]
+pub struct Preferences {
+    pub notifications: Notifications,
+    pub theme: String,
+    pub language: String,
+}
+
+#[derive(Debug, JsonDeserialise)]
+pub struct Notifications {
+    pub email: bool,
+    pub sms: bool,
+}
+
+#[derive(Debug, JsonDeserialise)]
+pub struct History {
+    pub login: String,
+    pub ip: String,
+    pub success: bool,
+}
+
+#[derive(Debug, JsonDeserialise)]
+pub struct Numbers {
+    pub int: i64,
+    pub float: f64,
+    pub scientific: f64,
+    pub scientific_no_decimal: f64,
+    pub negative: i64,
+    pub negative_scientific: f64,
 }
 
 fn main() {
-    let result: Result<Test, _> = Parser::parse(include_str!("test_data/test_blob.json"));
+    let result: Result<Root, _> = Parser::parse(include_str!("test_data/test_blob.json"));
     // let result = Test::parse_json(r#"{"name": "hi", "age": 3}"#);
     println!("{result:#?}");
 
